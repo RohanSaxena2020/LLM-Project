3864da326fd943ca9000c23ef53b18b27a42c951
RohanSaxena2020 <71786640+RohanSaxena2020@users.noreply.github.com>
2024-07-27 14:16:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
-st.title("AtliQ T Shirts: Database Q&A 👕")
+st.title("Advertisting Key Performance Indicators (KPIs) Q&A 📈")
 
 question = st.text_input("Question: ")
 
